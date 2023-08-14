bc3839e1d176c537d139d5b19dc8191cff78f343
alex <reidsgadv@gmail.com>
2023-08-14 19:09:23 +1000

    пр14

--- models/card.js
+++ models/card.js
@@ -4,13 +4,13 @@ const cardSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Название карточки обязательно к заполнению.'],
+      required: [true],
       minlength: 2,
       maxlength: 30,
     },
     link: {
       type: String,
-      required: [true, 'URL карточки обязателен к заполнению.'],
+      required: [true],
       validate: {
         validator(url) {
           return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(
--- routes/cards.js
+++ routes/cards.js
@@ -18,8 +18,8 @@ router.post(
   '/',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      link: Joi.string().pattern(urlRegex),
+      name: Joi.string().required().min(2).max(30),
+      link: Joi.string().required().pattern(urlRegex),
     }),
   }),
   postCard,
